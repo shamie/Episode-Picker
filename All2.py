e6c9b35e45eaceb76d3439c91dc9ccc8f3d18e7b
Jamie Sherlock <jamie.sherlock@gmail.com>
2011-07-22 01:47:08 -0700

    User chooses which series

---
+++ All2.py
@@ -0,0 +1,277 @@
+import random
+
+def TOS():
+	seasons = [1, 2, 3]
+	season1 = []
+	season2 = []
+	season3 = []
+
+	for i in range(1, 29):
+		season1.append(i)
+
+	for i in range(1, 26):
+		season2.append(i)
+
+	for i in range(1, 24):
+		season3.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	elif season == 2:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+	else:
+		random.shuffle(season3)
+		print "Episode: %d" % season3[1]
+		
+def TAS():
+	seasons = [1, 2]
+	season1 = []
+	season2 = []
+
+	for i in range(1, 16):
+		season1.append(i)
+
+	for i in range(1, 6):
+		season2.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	else:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+
+def TNG():
+	seasons = [1, 2, 3, 4, 5, 6, 7]
+	season1 = []
+	season2 = []
+	season3 = []
+	season4 = []
+	season5 = []
+	season6 = []
+	season7 = []
+
+	for i in range(1, 26):
+		season1.append(i)
+
+	for i in range(1, 22):
+		season2.append(i)
+
+	for i in range(1, 26):
+		season3.append(i)
+	
+	for i in range(1, 26):
+		season4.append(i)
+
+	for i in range(1, 26):
+		season5.append(i)
+
+	for i in range(1, 26):
+		season6.append(i)
+	
+	for i in range(1, 26):
+		season7.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	elif season == 2:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+	elif season == 3:
+		random.shuffle(season3)
+		print "Episode: %d" % season3[1]
+	elif season == 4:
+		random.shuffle(season4)
+		print "Episode: %d" % season4[1]
+	elif season == 5:
+		random.shuffle(season5)
+		print "Episode: %d" % season5[1]
+	elif season == 6:
+		random.shuffle(season6)
+		print "Episode: %d" % season6[1]
+	else:
+		random.shuffle(season7)
+		print "Episode: %d" % season7[1]
+
+def DS9():
+	seasons = [1, 2, 3, 4, 5, 6, 7]
+	season1 = []
+	season2 = []
+	season3 = []
+	season4 = []
+	season5 = []
+	season6 = []
+	season7 = []
+
+	for i in range(1, 20):
+		season1.append(i)
+
+	for i in range(1, 26):
+		season2.append(i)
+
+	for i in range(1, 26):
+		season3.append(i)
+
+	for i in range(1, 26):
+		season4.append(i)
+
+	for i in range(1, 26):
+		season5.append(i)
+
+	for i in range(1, 26):
+		season6.append(i)
+
+	for i in range(1, 26):
+		season7.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	elif season == 2:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+	elif season == 3:
+		random.shuffle(season3)
+		print "Episode: %d" % season3[1]
+	elif season == 4:
+		random.shuffle(season4)
+		print "Episode: %d" % season4[1]
+	elif season == 5:
+		random.shuffle(season5)
+		print "Episode: %d" % season5[1]
+	elif season == 6:
+		random.shuffle(season6)
+		print "Episode: %d" % season6[1]
+	else:
+		random.shuffle(season7)
+		print "Episode: %d" % season7[1]
+
+def VOY():
+	seasons = [1, 2, 3, 4, 5, 6, 7]
+	season1 = []
+	season2 = []
+	season3 = []
+	season4 = []
+	season5 = []
+	season6 = []
+	season7 = []
+
+	for i in range(1, 16):
+		season1.append(i)
+
+	for i in range(1, 26):
+		season2.append(i)
+
+	for i in range(1, 26):
+		season3.append(i)
+	
+	for i in range(1, 26):
+		season4.append(i)
+
+	for i in range(1, 26):
+		season5.append(i)
+
+	for i in range(1, 26):
+		season6.append(i)
+	
+	for i in range(1, 26):
+		season7.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	elif season == 2:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+	elif season == 3:
+		random.shuffle(season3)
+		print "Episode: %d" % season3[1]
+	elif season == 4:
+		random.shuffle(season4)
+		print "Episode: %d" % season4[1]
+	elif season == 5:
+		random.shuffle(season5)
+		print "Episode: %d" % season5[1]
+	elif season == 6:
+		random.shuffle(season6)
+		print "Episode: %d" % season6[1]
+	else:
+		random.shuffle(season7)
+		print "Episode: %d" % season7[1]
+
+def ENT():
+	seasons = [1, 2, 3, 4]
+	season1 = []
+	season2 = []
+	season3 = []
+	season4 = []
+
+	for i in range(1, 25):
+		season1.append(i)
+
+	for i in range(1, 26):
+		season2.append(i)
+
+	for i in range(1, 24):
+		season3.append(i)
+	
+	for i in range(1, 22):
+		season4.append(i)
+
+	random.shuffle(seasons)
+	season = seasons[1]
+	print "Season: %d" % season
+
+	if season == 1:
+		random.shuffle(season1)
+		print "Episode: %d" % season1[1]
+	elif season == 2:
+		random.shuffle(season2)
+		print "Episode: %d" % season2[1]
+	elif season == 3:
+		random.shuffle(season3)
+		print "Episode: %d" % season3[1]
+	else:
+		random.shuffle(season4)
+		print "Episode: %d" % season4[1]
+		
+series = raw_input("> TOS, TAS, TNG, DS9, VOY, or ENT \n> ")
+
+print "Series: " + series
+
+if series == "TOS":
+	TOS()
+elif series == "TAS":
+	TAS()
+elif series == "TNG":
+	TNG()
+elif series == "DS9":
+	DS9()
+elif series == "VOY":
+	VOY()
+else:
+	ENT()
+
